ef4f86e8dbba1fe1e6f4709a2345edddf3bbf3b8
skay138 <nine1352@gmail.com>
2023-05-10 00:02:41 +0900

    post&test_model

--- apis/models.py
+++ apis/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Test(models.Model):
+    name = models.TextField(
+        max_length=20,
+        default='anonymous',
+    )
+    age = models.IntegerField(
+        null=True
+    )
+    title = models.TextField(
+        null=False
+    )
+    context = models.TextField(
+        max_length=255
+    )
--- apis/views.py
+++ apis/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render
 from django.http import response
+from rest_framework.decorators import api_view
+from .models import Test
+from rest_framework import serializers
 
 # Create your views here.
 
+class testSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Test
+        fields = '__all__'
 
+@api_view(['GET', 'POST'])
 def apis_view(request):
     if request.method == 'GET':
-        data = {
-            "author" : "sangkyu",
-            "content" : "he is studying django"
-        }
-        return response.JsonResponse(data, safe=False)
+        data = Test.objects.all()[:5]
+        serializer = testSerializer(data, many=True)
+        return response.JsonResponse(serializer.data, safe=False, status=200)
     
+    if request.method == 'POST':
+        try:
+            test = Test.objects.create()
+            for keys in request.data:
+                if  hasattr(test, keys) == True:
+                    setattr(test, keys, request.data[keys])
+            test.save()
+            return response.HttpResponse(status=201)
+        except:
+            return response.HttpResponse(status=400)
